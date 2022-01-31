3850fde3d8772c40ac3bb3f03d9844b6426228b1
APARNA <aparnashetty2012@gmail.com>
2022-01-31 11:58:39 +0530

    added badges

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shppingweb'`, () => {
+  it(`should have as title 'Shopping_website'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shppingweb');
+    expect(app.title).toEqual('Shopping_website');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shppingweb app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Shopping_website app is running!');
   });
 });
