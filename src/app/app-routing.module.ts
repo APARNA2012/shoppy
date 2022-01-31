32434e9664829a7f5199fa75f9afa9d8cc44b6a6
APARNA <aparnashetty2012@gmail.com>
2022-01-31 20:30:07 +0530

    Custom badges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {MatBadgeModule} from '@angular/material/badge'
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),MatBadgeModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
