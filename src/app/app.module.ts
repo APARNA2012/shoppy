4dbfc2bc4dcef7a6603a4624b73e931995364e18
APARNA2012 <56128531+APARNA2012@users.noreply.github.com>
2022-02-17 12:21:49 +0530

    Revert "Dev/header"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,9 @@ import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module'
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-import { WeatherHeaderComponent } from './weather-header/weather-header.component'
-import { WeatherSearchBarComponent } from './weather-search-bar/weather-search-bar.component'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    WeatherHeaderComponent,
-    WeatherSearchBarComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
