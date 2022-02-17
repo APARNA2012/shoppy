e637c9098c01caa09e22bb417d13b855606ff6bf
APARNA <aparnashetty2012@gmail.com>
2022-02-17 12:08:35 +0530

    weather header

--- src/app/weather-header/weather-header.component.ts
+++ src/app/weather-header/weather-header.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-weather-header',
   templateUrl: './weather-header.component.html',
   styleUrls: ['./weather-header.component.scss'],
 })
-export class WeatherHeaderComponent implements OnInit {
+export class WeatherHeaderComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
